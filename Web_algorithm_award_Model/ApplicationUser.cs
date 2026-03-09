3fc80e5c503a519d68eb4ffa3f293a6de1603061
MinhND <minhnd@fint.vn>
2026-03-09 20:51:16 +0700

    commit

--- Web_algorithm_award_Model/ApplicationUser.cs
+++ Web_algorithm_award_Model/ApplicationUser.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Web_algorithm_award_Model
 {
@@ -12,10 +7,13 @@ namespace Web_algorithm_award_Model
     {
         [Required]
         public string? Name { get; set; }
+
         [Display(Name = "Địa chỉ nhà")]
         public string? HomeAddress { get; set; }
+
         public string? PostalCode { get; set; }
+
         [Display(Name = "Số điện thoại")]
-        override public string? PhoneNumber { get; set; }
+        public override string? PhoneNumber { get; set; }
     }
 }
